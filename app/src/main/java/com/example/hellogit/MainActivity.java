fd870706677c917a8e90b0fa79e07b66216f247c
Babulalkumar <babu38897@gmail.com>
2023-09-11 11:24:58 +0530

    hello git Gin

--- app/src/main/java/com/example/hellogit/MainActivity.java
+++ app/src/main/java/com/example/hellogit/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView (R.layout.activity_main);
 
         String Nmw="hello kja";
+        String Nmw2="hello Git again";
     }
 }
